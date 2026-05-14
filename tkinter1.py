f3b316255c86625d8d2ca645559bf51cbc21367e
d-adz <demiadewole0@gmail.com>
2026-05-14 22:03:29 +0100

    converted to class

--- tkinter1.py
+++ tkinter1.py
@@ -1,38 +1,44 @@
 from tkinter import *
 from tkinter import ttk
 
+class FeetToMetres:
+
+    def __init__(self,root):
+        root.title("Feet to Metres")
+
+        mainframe = ttk.Frame(root,padding= (3,3,12,12))
+        mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
+
+        self.feet = StringVar()
+        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
+        feet_entry.grid(column=2, row=1, sticky=(W,E))
+
+        self.meters = StringVar()
+        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
+
+        ttk.Button(mainframe, text="Calculate", command= self.calculate).grid(column=3, row=3, sticky=W)
+
+        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)	
+        mainframe.columnconfigure(2, weight=1)
+        for child in mainframe.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+        feet_entry.focus()
+        root.bind("<Return>", self.calculate)
+
+    def calculate(self,*args):
+        try:
+            value = float(self.feet.get())
+            self.meters.set(round(0.3048 * value, 4))
+        except ValueError:
+            pass
+
+
+
 root = Tk()
-root.title("Feet to Metres")
-
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(round(0.3048 * value, 4))
-    except ValueError:
-        pass
-
-mainframe = ttk.Frame(root,padding= (3,3,12,12))
-mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
-
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W,E))
-
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-
-ttk.Button(mainframe, text="Calculate", command= calculate).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)	
-mainframe.columnconfigure(2, weight=1)
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-feet_entry.focus()
-root.bind("<Return>", calculate)
-
+FeetToMetres(root)
 root.mainloop()
